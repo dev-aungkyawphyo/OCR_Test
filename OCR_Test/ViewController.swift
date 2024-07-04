371f2d83ac3676cc7853c2cbbb3308b304b4737d
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-05 00:56:59 +0630

    Initial Commit

---
+++ OCR_Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCR_Test
+//
+//  Created by Aung Kyaw Phyo on 05/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
